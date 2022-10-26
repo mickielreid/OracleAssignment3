8664b47bbc2f28aea653ce7428fb61a38cd29b09
glad2os <45284275+glad2os@users.noreply.github.com>
2022-10-25 23:06:04 -0400

    Updating 3rd question

--- OracleAssignment3.sql
+++ OracleAssignment3.sql
@@ -115,10 +115,10 @@ RESULTS:
 Q3. Creating a Procedure
 Create a procedure named DDCKPAY_SP that confirms whether a monthly pledge payment is
 the correct amount. The procedure needs to accept two values as input: a payment amount and a
-pledge ID. Based on these inputs, the procedure should confirm that the payment is∑the correct
+pledge ID. Based on these inputs, the procedure should confirm that the payment is¬∑the correct
 monthly Increment amount, based on pledge data in the database. If It isn't, a custom Oracle error
-using error number 20050 and the message "Incorrect payment amount - planned payment =??î
-should be raised. The ì??î should be replaced by the correct payment amount.
+using error number 20050 and the message "Incorrect payment amount - planned payment =??‚Äù
+should be raised. The ‚Äú??‚Äù should be replaced by the correct payment amount.
 The database query in the procedure should be formulated so that no rows are returned if the
 pledge isn't on a monthly payment plan or the pledge isn't found. If the query returns no rows,
 the procedure should display the message "No payment Information."
@@ -135,46 +135,45 @@ TESTING WITH ID = 100 AND PAYMENT = $23
  
 
 CODE:*/
-CREATE OR REPLACE PROCEDURE DDCKPAY_SP 
-  (P_ID IN NUMBER, 
-   P_AMT IN NUMBER,
-   RESPONCE OUT VARCHAR2)
- IS
-  M_MONTH DD_PLEDGE.PAYMONTHS%TYPE ;
-  M_ID    DD_PLEDGE.IDPLEDGE%TYPE;
-  M_AMT   DD_PLEDGE.PLEDGEAMT%TYPE;
-   FINAL_AMT   DD_PLEDGE.PLEDGEAMT%TYPE;
-  NO_MONTH EXCEPTION;
-  
-  
+
+CREATE OR REPLACE PROCEDURE DDCKPAY_SP(DD_ID IN NUMBER,
+                                       DD_AMT IN NUMBER,
+                                       OUTPUT OUT VARCHAR2)
+    IS
+
+    LV_ROW      DD_PLEDGE%ROWTYPE;
+    FINAL_PRICE NUMBER;
+    INCORRECT EXCEPTION;
 BEGIN
-        SELECT IDPLEDGE , PLEDGEAMT , PAYMONTHS
-        INTO   M_ID , M_AMT , M_MONTH
-        FROM DD_PLEDGE
-        WHERE IDPLEDGE = P_ID;
-        
-    IF M_MONTH = 0 THEN 
-        RAISE NO_MONTH;
-    END IF;
-    
-    
-      FINAL_AMT :=  M_AMT /M_MONTH;
-    
-    IF P_AMT = FINAL_AMT THEN
-        RESPONCE := 'CORRECT PAYMENT';
-    ELSIF P_AMT != FINAL_AMT THEN 
-        RAISE_APPLICATION_ERROR(-20050 , 'Incorrect payment amount - planned payment = ' || FINAL_AMT);
+    SELECT *
+    INTO LV_ROW
+    FROM DD_PLEDGE
+    WHERE IDPLEDGE = DD_ID;
+
+
+    IF LV_ROW.PAYMONTHS = 0 THEN
+        RAISE INCORRECT;
     END IF;
 
-  EXCEPTION
-  WHEN NO_DATA_FOUND THEN
-    DBMS_OUTPUT.PUT_LINE('No payment information');
-  WHEN NO_MONTH THEN 
-    DBMS_OUTPUT.PUT_LINE('No payment information');
+    FINAL_PRICE := LV_ROW.PLEDGEAMT / LV_ROW.PAYMONTHS;
+
+    IF DD_AMT = FINAL_PRICE THEN
+        OUTPUT := 'THE PAYMENT IS CORRECT';
+    ELSIF DD_AMT != FINAL_PRICE THEN
+        RAISE_APPLICATION_ERROR(-20050, 'Incorrect payment amount - planned payment = ' || FINAL_PRICE);
+    END IF;
+
+EXCEPTION
+    WHEN INCORRECT THEN
+        DBMS_OUTPUT.PUT_LINE('No payment information');
 END DDCKPAY_SP;
 
-
-
+DECLARE
+    OUTPUT VARCHAR2(242);
+BEGIN
+    DDCKPAY_SP(104, 20, OUTPUT);
+    DBMS_OUTPUT.PUT_LINE(OUTPUT);
+END;
 
 /*
 
